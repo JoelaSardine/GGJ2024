f234956ca6d6b861cc9405da4e6674717ce1b130
guillaume <guillaume@trebuchet.fun>
2024-01-27 18:30:37 -0500

    plein de  truc

---
+++ GGJ2024Project/Assets/Scripts/Alien/Alien.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using DG.Tweening;
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    public Mood BaseMood;
+    public List<Sequence> Sequences;
+
+    [Header("Reference")]
+    [SerializeField] private SpriteRenderer renderer;
+    [SerializeField] private SequenceVerifier verifier;
+    [SerializeField] private Mood leavingMood;
+    
+    private Transform OutPoint;
+
+    public void Init(Transform inPoint, Transform wait, Transform outPoint)
+    {
+        transform.position = inPoint.position;
+        OutPoint = outPoint;
+        SetMood(BaseMood);
+        verifier.SetSequences(Sequences);
+        verifier.OnSequenceValidated.AddListener(SetMood);
+        transform.DOMove(wait.position, 5.0f);
+    }
+    
+    public void SetMood(Mood mood)
+    {
+        renderer.sprite = mood.MoodSprite;
+
+        if (mood == leavingMood)
+        {
+            Leave();
+        }
+    }
+
+    private void Leave()
+    {
+        transform.DOMove(OutPoint.position, 5.0f).SetDelay(2.0f).onComplete = () => {Destroy(gameObject);};
+        verifier.OnSequenceValidated.RemoveListener(SetMood);
+    }
+}
---
+++ GGJ2024Project/Assets/Scripts/Alien/Mood.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Mood", menuName = "GGJ24/Mood", order = 1)]
+public class Mood : ScriptableObject
+{
+  public Sprite MoodSprite;
+}
