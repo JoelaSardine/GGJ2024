0f5c79486b844cf7f139822229addd2de3a0af86
Thibaut Poittevin <tpoittevin@amplitude-studios.com>
2024-01-28 00:23:29 +0100

    Split AlienDefinitions

---
+++ GGJ2024Project/Assets/Scripts/Alien/CultureDefinition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCultureDefinition", menuName = "GGJ24/CultureDefinition", order = 1)]
+public class CultureDefinition : ScriptableObject
+{
+	public string Name;
+	public CulturalFeature[] Features;
+}
+
+[System.Serializable]
+public class CulturalFeature
+{
+	// Ex : Red Face Painting
+	public string Name;
+	public CulturalSlot Slot;
+	public CulturalTrait[] Traits;
+
+	public Texture Texture;
+}
---
+++ GGJ2024Project/Assets/Scripts/Alien/RaceDefinition.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewRaceDefinition", menuName = "GGJ24/RaceDefinition", order = 1)]
+public class RaceDefinition : ScriptableObject
+{
+	public string Name;
+	public RacialFeature[] Features;
+
+
+	[System.Serializable]
+	public class RacialFeature
+	{
+		// Ex : 2 Big Yellow Noses
+		public string Name;
+		public RacialSlot Slot;
+		public RacialTrait[] Traits;
+
+		public Texture Texture;
+	}
+}
